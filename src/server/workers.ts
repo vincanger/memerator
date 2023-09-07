a31a9f055d2b4016909d3b59b2f7ddda3c468426
vincanger <70215737+vincanger@users.noreply.github.com>
2023-09-07 11:05:52 +0200

    Update workers.ts

--- src/server/workers.ts
+++ src/server/workers.ts
@@ -3,20 +3,26 @@ import axios from 'axios';
 export const fetchAndStoreMemeTemplates = async (_args: any, context: any) => {
   console.log('.... ><><>< get meme templates cron starting ><><>< ....');
 
-  const response = await axios.get('https://api.imgflip.com/get_memes');
+  try {
+    const response = await axios.get('https://api.imgflip.com/get_memes');
 
-  response.data.data.memes.forEach(async (meme: any) => {
-    await context.entities.Template.upsert({
-      where: { id: meme.id },
-      create: {
-        id: meme.id,
-        name: meme.name,
-        url: meme.url,
-        width: meme.width,
-        height: meme.height,
-        boxCount: meme.box_count,
-      },
-      update: {},
+    const promises = response.data.data.memes.map((meme: any) => {
+      return context.entities.Template.upsert({
+        where: { id: meme.id },
+        create: {
+          id: meme.id,
+          name: meme.name,
+          url: meme.url,
+          width: meme.width,
+          height: meme.height,
+          boxCount: meme.box_count,
+        },
+        update: {},
+      });
     });
-  });
+
+    await Promise.all(promises);
+  } catch (error) {
+    console.error('error fetching meme templates: ', error);
+  }
 };
